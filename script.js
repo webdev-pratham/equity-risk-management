f285918a6874d5f04a3d8be7cd868d763814b642
Prathamesh Bardiya <122727701+webdev-pratham@users.noreply.github.com>
2023-09-24 00:43:31 +0530

    2 Update script.js

--- script.js
+++ script.js
@@ -11,16 +11,18 @@ BTN.addEventListener("click", A);
 	let t2 = document.getElementById("T2");
 	let t3 = document.getElementById("T3");
 	let t4 = document.getElementById("T4");
-	
+
 	let slps = Entry - SL;
 
 	n4.innerText = Entry - SL;
 	TST.innerText = Risk / (Entry - SL);
 
-	t1.innerText = parseInt(-1*(- Entry - slps));
-	t2.innerText = parseInt(-1*(- Entry - (slps * 2)));
-	t3.innerText = parseInt(-1*(- Entry - (slps * 3)));
-	t4.innerText = parseInt(-1*(- Entry - (slps * 5)));
+	t1.innerText = (-1 * (- Entry - slps));
+	t2.innerText = (-1 * (- Entry - (slps * 2)));
+	t3.innerText = (-1 * (- Entry - (slps * 3)));
+	t4.innerText = (-1 * (- Entry - (slps * 5)));
 
+	let cap = document.getElementById("capital");
+
+	cap.innerText =parseInt( Entry * TST.innerText);
 }
-
