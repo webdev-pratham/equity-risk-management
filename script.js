6bcf7dea402f63fe49aeb763c58ff6880b046e4d
Prathamesh Bardiya <122727701+webdev-pratham@users.noreply.github.com>
2023-09-24 00:20:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let BTN = document.getElementById("calc");
+
+BTN.addEventListener("click", A);
+
+ function A () {
+   let Risk = document.getElementById("risk").value;
+ 	let Entry = document.getElementById("entry").value;
+   let SL = document.getElementById("sl").value;
+	let TST = document.getElementById("tst");	
+	let t1 = document.getElementById("T1");
+	let t2 = document.getElementById("T2");
+	let t3 = document.getElementById("T3");
+	let t4 = document.getElementById("T4");
+	
+	let slps = Entry - SL;
+
+	n4.innerText = Entry - SL;
+	TST.innerText = Risk / (Entry - SL);
+
+	t1.innerText = parseInt(-1*(- Entry - slps));
+	t2.innerText = parseInt(-1*(- Entry - (slps * 2)));
+	t3.innerText = parseInt(-1*(- Entry - (slps * 3)));
+	t4.innerText = parseInt(-1*(- Entry - (slps * 5)));
+
+}
+
